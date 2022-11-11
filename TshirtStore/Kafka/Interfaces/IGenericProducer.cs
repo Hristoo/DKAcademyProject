3b4d36694a03be9d7551be974e712b5e991241b4
Hristo Dimitrov <hr.dimitrov@draftkings.com>
2022-11-11 16:37:10 +0200

    add shopping cart in tests

---
+++ TshirtStore/Kafka/Interfaces/IGenericProducer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using static MongoDB.Driver.WriteConcern;
+using System.Threading.Tasks;
+
+namespace Kafka.Interfaces
+{
+    public interface IGenericProducer<TKey, TValue>
+    {
+        Task SendMessage(TKey key, TValue value);
+    }
+}
